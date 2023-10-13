dffaa92b435198afae5d4ed3b0a8f63927743b9a
mnadeem8200 <146753235+mnadeem8200@users.noreply.github.com>
2023-10-14 00:48:35 +0500

    All files done ready to upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,18 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = 10; // Change this value to set the number of Fibonacci terms to display
+
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
