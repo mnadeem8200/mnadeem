5776b6221ad000e023389d243082080ed587b3a2
mnadeem8200 <146753235+mnadeem8200@users.noreply.github.com>
2023-10-15 21:09:40 +0500

    My New file upload

---
+++ SumCalculator.java
@@ -0,0 +1,13 @@
+public class SumCalculator {
+    public static void main(String[] args) {
+        // Declare and initialize two numbers
+        int number1 = 5;
+        int number2 = 7;
+
+        // Calculate the sum of the two numbers
+        int sum = number1 + number2;
+
+        // Display the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
